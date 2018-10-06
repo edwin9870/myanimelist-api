7bea450d6c3f29155874e71f27c0ab0b6eaf4988
Edwin Ramirez Ventura <edwin9870@gmail.com>
2018-10-06 18:32:27 -0400

    Create AnimeRepository

---
+++ src/main/kotlin/com/edwin/myanimelist/data/repositories/AnimeRepository.kt
@@ -0,0 +1,8 @@
+package com.edwin.myanimelist.data.repositories
+
+import com.edwin.myanimelist.data.entities.Anime
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface AnimeRepository: MongoRepository<Anime, String>
